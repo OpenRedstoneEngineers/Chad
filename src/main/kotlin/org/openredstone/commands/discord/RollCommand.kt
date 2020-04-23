b70ba3ee8d5f647064187fb6901d79db21a54cdf
Pauli Kauro <3965357+paulikauro@users.noreply.github.com>
2020-04-23 19:24:48 +0300

    Use array random extension function

--- src/main/kotlin/org/openredstone/commands/discord/RollCommand.kt
+++ src/main/kotlin/org/openredstone/commands/discord/RollCommand.kt
@@ -1,11 +1,9 @@
 package org.openredstone.commands.discord
 
-import kotlin.random.Random
-
 class RollCommand : DiscordCommand("roll") {
     private val dice = arrayOf("⚀", "⚁", "⚂", "⚃", "⚄", "⚅")
 
     override fun runCommand(args: Array<String>) {
-        reply = dice[Random.nextInt(0, 5)]
+        reply = dice.random()
     }
 }
