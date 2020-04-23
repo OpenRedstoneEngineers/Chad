a0a569968c030a961ed49ad85fca53a358c24ac5
Pauli Kauro <3965357+paulikauro@users.noreply.github.com>
2020-04-23 21:06:12 +0300

    Reduce nesting in ListCommand

--- src/main/kotlin/org/openredstone/commands/irc/ListCommand.kt
+++ src/main/kotlin/org/openredstone/commands/irc/ListCommand.kt
@@ -1,25 +1,30 @@
 package org.openredstone.commands.irc
 
 import org.javacord.api.DiscordApi
+import java.util.*
 
 class ListCommand(private var statusChannelId: Long, private var discordApi: DiscordApi)
     : IrcCommand("list", 0, true) {
     override fun runCommand(args: List<String>) {
         val stringBuilder = StringBuilder()
-        discordApi.getServerTextChannelById(statusChannelId).ifPresent { channel ->
-            channel.getMessages(1).get().forEach { message ->
-                message.embeds[0].fields.forEach {
-                    if (it.isInline) {
-                        stringBuilder.append(it.name.replace("*",""))
-                        if (!it.name.contains("offline")
-                            && !it.name.replace("*","").contains("(0)")) {
-                            stringBuilder.append(": ").append(it.value.replace("`",""))
-                        }
-                        stringBuilder.append("\n")
-                    }
+        val channel = discordApi.getServerTextChannelById(statusChannelId).toNullable() ?: return
+        channel.getMessages(1).get().first()
+            .embeds[0].fields.asSequence()
+            .filter { it.isInline }
+            .forEach {
+                val name = it.name.replace("*", "")
+                val value = it.value.replace("`", "")
+                stringBuilder.append(name)
+                if (!name.contains("offline") && !name.contains("(0)")) {
+                    stringBuilder.append(": ").append(value)
                 }
+                stringBuilder.append("\n")
             }
-        }
         reply = stringBuilder.toString()
     }
+}
+
+fun <T> Optional<T>.toNullable(): T? = when {
+    isPresent -> get()
+    else -> null
 }
