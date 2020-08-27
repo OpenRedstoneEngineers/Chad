d51d10fcae713801d4f6fd9caf0a5008e10d5aec
Pauli Kauro <3965357+paulikauro@users.noreply.github.com>
2020-08-27 23:01:07 +0300

    Upgrade to Kotlin 1.4.0 and Gradle 6.6.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     application
     java
-    kotlin("jvm") version "1.3.72"
+    kotlin("jvm") version "1.4.0"
     id("com.github.johnrengelman.shadow") version "5.2.0"
 }
 
