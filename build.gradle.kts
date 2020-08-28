2ec717640ca46d15606b75d053a8d3e9fdb1bb97
konsumlamm <konsumlamm@gmail.com>
2020-08-28 13:22:36 +0200

    Update dependencies
    Update shadowjar plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     application
     java
     kotlin("jvm") version "1.4.0"
-    id("com.github.johnrengelman.shadow") version "5.2.0"
+    id("com.github.johnrengelman.shadow") version "6.0.0"
 }
 
 group = "org.openredstone"
@@ -24,17 +24,16 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
     implementation(group = "com.github.jkcclemens", name = "khttp", version = "0.1.0")
     implementation(group = "org.jsoup", name = "jsoup", version = "1.13.1")
-    implementation(group = "org.javacord", name = "javacord", version = "3.0.3")
+    implementation(group = "org.javacord", name = "javacord", version = "3.0.6")
     implementation(group = "com.uchuhimo", name = "konf", version = "0.22.1")
     implementation(group = "org.pircbotx", name = "pircbotx", version = "2.1")
-    implementation(group = "io.github.microutils", name = "kotlin-logging", version = "1.7.9")
-    runtime(group = "org.slf4j", name = "slf4j-simple", version = "1.7.29")
+    implementation(group = "io.github.microutils", name = "kotlin-logging", version = "1.8.3")
+    runtimeOnly(group = "org.slf4j", name = "slf4j-simple", version = "1.7.30")
 
     testImplementation(kotlin("test-junit5"))
-    testRuntime(group = "org.junit.jupiter", name = "junit-jupiter-engine", version = "5.6.2")
+    testRuntimeOnly(group = "org.junit.jupiter", name = "junit-jupiter-engine", version = "5.6.2")
 }
 
 tasks {
